0005d957a281ef05f0289eee8f91d82f8d220bb2
rbasehewa <rbasehewa@gmail.com>
2021-09-20 11:17:42 +1000

    adding dec and binding, DI comments

--- src/app/component/shopping-cart/product-list/product-item/product-item.component.ts
+++ src/app/component/shopping-cart/product-list/product-item/product-item.component.ts
@@ -10,17 +10,23 @@ import { MessengerService } from 'src/app/services/messenger.service';
 })
 export class ProductItemComponent implements OnInit {
 
+  //this is input decorator as parent component which we can access our template
   @Input() productItem: Product;
-  
+
   constructor(private msg: MessengerService) { }
 
   ngOnInit(): void {
     // this.ProductService.getProducts();
   }
 
+  /**
+ * This is handleAddtocart function executed trigger event when click add to cart 
+ */
+
   handleAddToCart(){
-    this.msg.sendMessage(this.productItem)
-    //console.log(this.productItem);
-  }
+    // send the message with the current product item inside the product item component
+  this.msg.sendMessage(this.productItem)
+    
+}
 
 }
--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,3 +1,8 @@
+
+/**
+ * This is the Product class for creating new object for possibilites from errors 
+ * help us for data types
+ */
 export class Product {
     id: number
     name: string
